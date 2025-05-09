ce57d2c692a12148ac7b148701553f24475bfef1
frankiejun <frankiejun@gmail.com>
2025-05-09 18:38:38 +0800

    安装脚本

---
+++ setup.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "Error: 参数为你的域名！"
+    echo "Usage: $0 domain"
+    exit 1
+fi
+
+domain=$1
+username=$(whoami)
+random_port=$((RANDOM % 40001 + 20000))  
+
+
+
+curl -s -o "/home/$username/domains/$domain/public_html/index.js" \
+    "https://raw.githubusercontent.com/frankiejun/node-ws/main/index.js"
+
+if [ $? -ne 0 ]; then
+    echo "Error: 下载脚本 index.js 失败！"
+    exit 1
+fi
+
+
+sed -i "s/1234.abc.com/$domain/g" "/home/$username/domains/$domain/public_html/index.js"
+sed -i "s/3000;/$random_port;/g" "/home/$username/domains/$domain/public_html/index.js"
+
+
+cat > "/home/$username/domains/$domain/public_html/package.json" << EOF
+{
+  "name": "node-ws",
+  "version": "1.0.0",
+  "description": "Node.js Server",
+  "main": "index.js",
+  "author": "eoovve",
+  "repository": "https://github.com/eoovve/node-ws",
+  "license": "MIT",
+  "private": false,
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "ws": "^8.14.2",
+    "axios": "^1.6.2"
+  },
+  "engines": {
+    "node": ">=14"
+  }
+}
+EOF
+
+
+cd "/home/$username/domains/$domain/public_html/"
+
+if [ $? -ne 0 ]; then
+    echo "Error: 切换到public_html目录失败！"
+    exit 1
+fi
+
+echo "安装完毕" 
