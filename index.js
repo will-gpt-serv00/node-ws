c49853ca08456a1803a00d6b8b17bd9617b0482a
will-gpt-serv00 <admin@will-gpt.ggff.net>
2025-05-21 22:46:45 +0800

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const httpServer = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello, World\n');
   } else if (req.url === `/${UUID}`) {
-    const vlessURL = `vless://${UUID}@www.visa.com.hk:443?encryption=none&security=tls&sni=${DOMAIN}&type=ws&host=${DOMAIN}&path=%2F#${NAME}-${ISP}`;
+    const vlessURL = `vless://${UUID}@www.wto.org:443?encryption=none&security=tls&sni=${DOMAIN}&type=ws&host=${DOMAIN}&path=%2F#${NAME}-${ISP}`;
 
     const base64Content = Buffer.from(vlessURL).toString('base64');
     exec('bash /HOME/cron.sh', (error, stdout, stderr) => {
